43b355e303d4e47355a603dd701deed2c0b4b8f4
Turbobus <59963570+Turbobus@users.noreply.github.com>
2022-02-15 12:04:34 +0100

    Will it work?

--- RealLab2/client.erl
+++ RealLab2/client.erl
@@ -78,13 +78,15 @@ handle(St, {leave, Channel}) ->             % Look over and see if we can remove
 % Sending message (from GUI, to channel)
 handle(St, {message_send, Channel, Msg}) ->
     % TODO: Implement this function
-   
-    Result = genserver:request(list_to_atom(Channel), {message_send, Channel, St#client_st.nick, Msg, self()}),
-
-    case Result of
-        ok -> {reply,ok,St};
-        failed -> {reply, {error, user_not_joined, "User not in Channel"}, St};
-        {'EXIT', _} -> {reply, {error, server_not_reached, "Server does not respond"}, St}
+    case lists:member(list_to_atom(Channel), registered()) of
+    
+        true -> Result = genserver:request(list_to_atom(Channel), {message_send, Channel, St#client_st.nick, Msg, self()}),
+                case Result of
+                    ok -> {reply,ok,St};
+                    failed -> {reply, {error, user_not_joined, "User not in Channel"}, St};
+                    {'EXIT',_} -> {reply, {error, server_not_reached, "Server does not respond"}, St}
+                end;
+        false -> {reply, {error, server_not_reached, "Server does not respond"}, St}
     end;
 
 
